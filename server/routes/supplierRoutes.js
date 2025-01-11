35620f7292d76b610f331edb8c3036963abc1d0e
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-11 21:40:22 +0530

    Updated supplierRoutes.js to reflect new API endpoints

--- server/routes/supplierRoutes.js
+++ server/routes/supplierRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addSupplier, getSuppliers } from "../controllers/supplierController.js";
+import { addSupplier, getSuppliers ,deleteSupplier } from "../controllers/supplierController.js";
 
 const router = express.Router();
 
 router.post("/", addSupplier); // Add a new supplier
 router.get("/", getSuppliers); // Get all suppliers
+router.delete("/:id", deleteSupplier);
 
 export default router;
