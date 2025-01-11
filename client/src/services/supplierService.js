e404c49f4f1b49700423a32d7d6b8f691bf40d17
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-11 21:40:21 +0530

    Updated supplierService.js with improved API endpoints handling

--- client/src/services/supplierService.js
+++ client/src/services/supplierService.js
@@ -1,4 +1,6 @@
-import api from "./api";
+import api from "./api"; // Use the axios instance you've created
+
+const API_URL = "suppliers";
 
 export const addSupplier = async (supplierData) => {
   const response = await api.post("/suppliers", supplierData);
@@ -7,5 +9,11 @@ export const addSupplier = async (supplierData) => {
 
 export const getSuppliers = async () => {
   const response = await api.get("/suppliers");
+  return response.data;
+};
+
+export const deleteSupplier = async (id) => {
+  // Use the `api` instance instead of directly calling `axios`
+  const response = await api.delete(`${API_URL}/${id}`);
   return response.data;
 };
